1d8b95915902bbea6ab83b013ed47bca17f66054
Christian Peralta <christian_94_97@hotmail.com>
2022-11-09 16:22:28 +0100

    setting md documentation

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
 from setuptools import setup, find_packages
 
 classifiers = [
-    'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Education',
     'Operating System :: Linux :: Ubuntu :: Kerner 5.15.0-52-generic',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3'
 ]
 
+with open("README.md", "r") as fh:
+    long_desciption = fh.read()
+
 setup(
     name='cheMLearning',
     version='0.0.1',
     description='Tools for Metabolomics Multivariate Analysis',
-    long_description=open('README.txt').read() + '\n\n' +
-    open('CHANGELOG.txt').read(),
+    long_description=long_desciption,
     url='',
     author='Christian Peralta',
     author_email='christian_94_97@hotmail.com',
     license='MIT',
     classifiers=classifiers,
-    keywords='calculator',
+    keywords='metabolomics',
     packages=find_packages(),
-    install_requires=['numpy', 'pandas',
-                      'scikit-learn', 'plotly', 'scipy', 'xlrd']
+    install_requires=['numpy >= 1.23.4',
+                      'pandas >= 1.5.1',
+                      'plotly >= 5.11.0',
+                      'scikit-learn >= 1.1.3',
+                      'scipy >= 1.9.3',
+                      'xlrd >= 2.0.1']
 )
